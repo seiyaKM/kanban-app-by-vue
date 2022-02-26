a52dd8c19bde6e887d83cb7255fc9576a60a5109
seiyaKM <seiya.kumada.0107@gmail.com>
2022-02-27 07:14:19 +0900

    9 page 348

---
+++ src/store/actions.js
@@ -0,0 +1,11 @@
+import * as types from "./mutation-type";
+import { Auth, List, Task } from "../api";
+
+export default {
+  login: ({ commit }) => {},
+  fetchList: ({ commit }) => {},
+  addTask: ({ commit }) => {},
+  updateTask: ({ commit }) => {},
+  removeTask: ({ commit }) => {},
+  logout: ({ commit }) => {}
+};
